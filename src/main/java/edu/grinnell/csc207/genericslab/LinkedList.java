d6e74541a692ce973e8bf844e91dfd531de87a90
David Lee <Cheongmin.gitlee@gmail.com>
2025-10-01 15:59:44 -0500

    Finished up the testing suite for LInkedList Class

--- src/main/java/edu/grinnell/csc207/genericslab/LinkedList.java
+++ src/main/java/edu/grinnell/csc207/genericslab/LinkedList.java
@@ -29,6 +29,67 @@ public class LinkedList<T> {
 
             cur.next = new Node<T>(value, null);
         }
+    }
 
+    public T get (int index){
+        Node<T> cur = first;
+        if (cur == null){
+            throw new IndexOutOfBoundsException();
+        } else if(index < 0){
+            throw new IndexOutOfBoundsException();
+        }
+        else {
+            for (int i = 0; i < index; i++){
+                if (cur.next == null){
+                    throw new IndexOutOfBoundsException();
+                }
+                cur = cur.next;
+            }
+            return cur.value;
+        }
+    }
+
+    public T remove(int index){
+        Node<T> cur = first;
+        T value;
+        if (cur == null){
+            throw new IndexOutOfBoundsException();
+        } else if (index < 0){
+            throw new IndexOutOfBoundsException();
+        } else if (index == 0){
+            value = first.value;
+            first = first.next;
+            return value;
+        } 
+        else {
+            for(int i = 0; i < index -1; i++){
+                if(cur.next == null){
+                    throw new IndexOutOfBoundsException();
+                }
+
+                cur = cur.next;
+            }
+
+            if(cur.next != null){
+                value = cur.next.value;
+                cur.next = cur.next.next;
+            } else {
+                throw new IndexOutOfBoundsException();
+            }
+
+            return value;
+        }
+    }
+
+    public int size(){
+        int counter = 0;
+        Node<T> cur = first;
+
+        while (cur != null){
+            counter++;
+            cur = cur.next;
+        }
+
+        return counter;
     }
 }
